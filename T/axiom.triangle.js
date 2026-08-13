34f25176b3ced5a670e3dfb6535fd8edc9e7d014
iki1uc <entertrainer@web.de>
2026-08-13 17:16:45 +0200

    Update axiom.triangle.js

--- T/axiom.triangle.js
+++ T/axiom.triangle.js
@@ -1,1 +1,33 @@
+export class AxiomTriangle {
+
+    constructor(){
+        this.tmp = [];
+    }
+
+    height(score){
+        const h = (score * 0.13) + Math.sin(score) * 3;
+        this.tmp.push(h);
+        return h;
+    }
+
+    width(score){
+        const w = (score * 0.17) + Math.cos(score) * 5;
+        this.tmp.push(w);
+        return w;
+    }
+
+    depth(score){
+        const d = (score * 0.19) + Math.tan(score % 3) * 2;
+        this.tmp.push(d);
+        return d;
+    }
+
+    triangle(score){
+        const h = this.height(score);
+        const w = this.width(score);
+        const d = this.depth(score);
+
+        return (h + w + d) * (this.tmp.length % 7 + 1);
+    }
+}
 

11bcf46cc00d22d89b93ad8745e086646c0541a2
iki1uc <entertrainer@web.de>
2026-08-13 17:18:24 +0200

    Update würfel.welt.js

--- sys/würfel.welt.js
+++ sys/würfel.welt.js
@@ -1,1 +1,18 @@
+import { RESPO_MATRIX } from "../B/RESPO_MATRIX.js";
+import { AxiomTriangle } from "../T/axiom.triangle.js";
+import { continuum } from "../T/würfel.continuum.js";
+
+export function WuerfelWelt(score){
+
+    const tri = new AxiomTriangle();
+    const triScore = tri.triangle(score);
+
+    const cont = continuum(triScore);
+
+    return {
+        matrix: RESPO_MATRIX,
+        triangle: triScore,
+        continuum: cont
+    };
+}
 

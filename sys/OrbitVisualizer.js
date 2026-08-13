5f28ac5499fcc74b9d6db60838ce725fe3292a29
iki1uc <entertrainer@web.de>
2026-08-13 17:17:31 +0200

    Add OrbitVisualizer function to create a sphere

--- sys/OrbitVisualizer.js
+++ sys/OrbitVisualizer.js
@@ -1,1 +1,11 @@
+export function OrbitVisualizer(scene){
+
+    const geo = new THREE.SphereGeometry(1.2, 32, 32);
+    const mat = new THREE.MeshBasicMaterial({ color: 0x00ff88, wireframe:true });
+    const sphere = new THREE.Mesh(geo, mat);
+
+    scene.add(sphere);
+
+    return sphere;
+}
 

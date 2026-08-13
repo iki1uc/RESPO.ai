51f91b314523e5fa5ac884aa9a6728a769c19b57
iki1uc <entertrainer@web.de>
2026-08-13 17:31:21 +0200

    Create tmp.legacy.js

---
+++ sys/tmp.legacy.js
@@ -0,0 +1,7 @@
+// Legacy‑System – alte Module bleiben erhalten
+
+window.RESPO_legacy = {
+    oldEngine: window.AxiomEngine,
+    oldTriangle: window.AxiomTriangle,
+    oldContinuum: window.continuum
+};
